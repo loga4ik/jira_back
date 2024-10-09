ea13bd8d9b252fb52cfa1ba55966b12b75fa5f53
loga4ik <logika7925@gmail.com>
2024-10-09 11:59:13 +0300

    seed

---
+++ db/seeders/20241009085307-status.js
@@ -0,0 +1,29 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert(
+      "TableName",
+      [
+        {
+          id: 2,
+          title: "новый",
+        },
+        {
+          id: 3,
+          title: "в работе",
+        },
+        {
+          id: 4,
+          title: "завершен",
+        },
+      ],
+      {}
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("TableName", null, {});
+  },
+};
